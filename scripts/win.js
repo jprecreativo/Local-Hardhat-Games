74c8f6793cc5fa1d9ab19b4f9684a2218be806c2
jprecreativo <jprecreativo@gmail.com>
2026-02-08 18:29:42 +0100

    Game1 won.

--- scripts/win.js
+++ scripts/win.js
@@ -1,6 +1,6 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
